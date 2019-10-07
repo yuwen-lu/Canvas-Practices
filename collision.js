5405d4b9de8d459b307c3ce354d859ed2582e43b
yuwen-lu <wayne.lu1998@gmail.com>
2019-10-08 00:15:54 +0200

    add hover change opacity functionality

--- collision.js
+++ collision.js
@@ -5,10 +5,20 @@ canvas.style.border = "solid 1px black";
 
 var c = canvas.getContext("2d");
 
-var circleAmount = 20;
-var gravity = 1;
-var fraction = 0.9;
-var colorArray = ["#1445D9", "#325CD9", "#203B8C", "#F29580", "#D91C0B"];
+const circleAmount = 200;
+// const gravity = 1;
+const fraction = 0.9;
+const distance = 50; // the activation distance of mouse to the circles
+const colorArray = ["#1445D9", "#325CD9", "#203B8C", "#F29580", "#D91C0B"];
+let mouse = {
+  x: undefined,
+  y: undefined
+};
+
+window.addEventListener("mousemove", e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
 
 function randomNumber(min, max) {
   if (min < max) {
@@ -30,18 +40,27 @@ function Circle(x, y, radius, dx, dy, color) {
   this.dx = dx;
   this.dy = dy;
   this.color = color;
+  this.opacity = 0; //initial opacity, 0
 
   this.Draw = function() {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    c.stroke();
+
+    c.save();
+    // the following line sets the global opacity of the canvas, which only affects the current
+    // circle cuz it's only filling the current circle.
+    c.globalAlpha = this.opacity;
     c.fillStyle = this.color;
     c.fill();
+    c.restore();
+
+    c.strokeStyle = this.color;
+    c.stroke();
     c.closePath();
   };
 
-  this.Update = function() {
-    // the following code is for moving and gravity
+  this.Update = circleArray => {
+    // the following code is for moving and (gravity)
     if (
       this.x + this.dx <= this.radius ||
       this.x + this.dx >= canvas.width - this.radius
@@ -56,7 +75,41 @@ function Circle(x, y, radius, dx, dy, color) {
     }
     this.x += this.dx;
     this.y += this.dy;
-    this.dy += gravity;
+    // this.dy += gravity;
+
+    // check if the circle is close to the mouse, if so increase opacity
+    if (
+      CircleDistance(this.x, this.y, mouse.x, mouse.y) <= distance &&
+      this.opacity < 0.4
+    ) {
+      this.opacity += 0.02;
+    }
+
+    if (
+      CircleDistance(this.x, this.y, mouse.x, mouse.y) > distance &&
+      this.opacity > 0
+    ) {
+      this.opacity -= 0.02;
+      this.opacity = Math.max(0, this.opacity);
+      console.log(this.opacity);
+    }
+
+    // check if the circle is colliding with other circles
+    circleArray.forEach(current => {
+      if (current !== this) {
+        // console.log(CircleDistance(this.x, current.x, this.y, current.y));
+        if (
+          CircleDistance(this.x, this.y, current.x, current.y) <=
+          this.radius + current.radius
+        ) {
+          //collide
+          this.dx = -this.dx;
+          this.dy = -this.dy;
+          current.dx = -current.dx;
+          current.dy = -current.dy;
+        }
+      }
+    });
 
     this.Draw();
   };
@@ -67,7 +120,6 @@ function CircleDistance(x1, y1, x2, y2) {
   let ydis = y1 - y2;
   return Math.sqrt(xdis * xdis + ydis * ydis);
 }
-console.log(CircleDistance(1, 2, 3, 3));
 
 var circleArray = [];
 
@@ -77,8 +129,8 @@ function initCircles(amount) {
 
     let x = randomNumber(radius, window.innerWidth - radius);
     let y = randomNumber(radius, window.innerHeight - radius);
-    var dx = randomNumber(-3, 3);
-    var dy = randomNumber(-3, 3);
+    var dx = randomNumber(-2, 2);
+    var dy = randomNumber(-2, 2);
     var color = colorArray[randomNumber(0, colorArray.length)];
 
     // if it's not the first circle, avoid it overlapping with any other circle
@@ -97,7 +149,6 @@ function initCircles(amount) {
 
     circleArray.push(new Circle(x, y, radius, dx, dy, color));
   }
-  console.log(circleArray);
 }
 
 function init() {
@@ -112,8 +163,8 @@ function animate() {
   c.clearRect(0, 0, canvas.width, canvas.height);
 
   circleArray.forEach(current => {
-    current.Update();
+    current.Update(circleArray);
   });
 }
 
-// animate();
+animate();
